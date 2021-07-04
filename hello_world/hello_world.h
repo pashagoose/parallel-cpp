5420f5171e3c1d8a44ca4c79faf6e48fdb5398c1
pasha <pavel.guskov02@mail.ru>
2021-07-04 20:10:59 +0300

    solved

--- hello_world/hello_world.h
+++ hello_world/hello_world.h
@@ -1,23 +1,36 @@
 #pragma once
 
+#include<mutex>
 #include <ostream>
 #include <string_view>
-
+#include<thread>
+#include<vector>
 
 class HelloWorld {
  public:
   static constexpr std::string_view kHelloPrefix = "Hello, World! From thread ";
 
-  HelloWorld(size_t /*n_threads*/)
+  HelloWorld(size_t n_threads) :
+                    n_threads_(n_threads) 
   {
-    // Your code
+    
   }
 
-  void SayHello(std::ostream& /*os*/) {
-    // Your code
+  void SayHello(std::ostream& os) {
+      std::vector<std::thread> threads;
+      for (size_t i = 0; i < n_threads_; ++i) {
+        threads.emplace_back([&] {
+          control_output.lock();
+          os << kHelloPrefix << std::this_thread::get_id() << '\n';
+          control_output.unlock();
+        });
+      }
+      for (auto& th : threads) {
+        th.join();
+      }
   }
 
  private:
-  // Your code
+  size_t n_threads_;
+  std::mutex control_output;
 };
-
