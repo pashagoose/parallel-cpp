6d396cfff81cb558f45f6f845740367a64f7733b
pasha <pavel.guskov02@mail.ru>
2021-07-06 22:57:41 +0300

    fixing

--- unbuffered_channel/unbuffered_channel.h
+++ unbuffered_channel/unbuffered_channel.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <condition_variable>
 #include <exception>
 #include <mutex>
@@ -14,58 +15,49 @@ class UnbufferedChannel{
 
   void Send(const T& value) {
       std::unique_lock locker(block_);
-      ++senders_pending_;
-      sending_message_.notify_one();
-      receiving_message_.wait(locker, [&] {
-        return (receivers_pending_ > 0) || closed_; 
+      ready_to_get_.wait(locker, [&] {
+        return (closed_ || receivers_pending_);
       });
       if (closed_) {
         throw std::runtime_error("Channel is closed");
       } else {
+        assert(!placed_);
+        placed_ = true;
         val_ = value;
-        --receivers_pending_;
-        placed_val_ = true;
-        placed_.notify_one();
+        placed_val_.notify_one();
       }
   }
 
   std::optional<T> Recv() {
-    std::unique_lock locker(block_);
-    sending_message_.wait(locker, [&] {
-      return (senders_pending_ > 0) || closed_;
-    });
-    if (closed_) {
-      return std::nullopt;
-    }
-    ++receivers_pending_;
-    receiving_message_.notify_one();
-    placed_.wait(locker, [&] {
-      return placed_val_ || closed_;
-    });
-    if (closed_) {
-      return std::nullopt;
-    }
-    --senders_pending_;
-    placed_val_ = false;
-    return val_;
+      std::unique_lock locker(block_);
+      ++receivers_pending_;
+      ready_to_get_.notify_one();
+      placed_val_.wait(locker, [&] {
+        return (closed_ || placed_);
+      });
+      if (closed_) {
+        return std::nullopt;
+      } else {
+        placed_ = false;
+        --receivers_pending_;
+        return val_;
+      }
   }
 
   void Close() {
     std::unique_lock locker(block_);
     closed_ = true;
-    sending_message_.notify_all();
-    receiving_message_.notify_all();
+    placed_val_.notify_all();
+    ready_to_get_.notify_all();
   }
 
  private:
   std::optional<T> val_;
   size_t receivers_pending_ = 0;
-  size_t senders_pending_ = 0;
   bool closed_ = false;
-  bool placed_val_ = false;
+  bool placed_ = false;
   std::mutex block_;
-  std::condition_variable sending_message_;
-  std::condition_variable receiving_message_;
-  std::condition_variable placed_;
+  std::condition_variable placed_val_;
+  std::condition_variable ready_to_get_;
 };
 
