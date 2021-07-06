524298971770c7192c8b0fc3901736db45758896
pasha <pavel.guskov02@mail.ru>
2021-07-06 23:37:24 +0300

    solved all issues

--- thread_safe_vector/thread_safe_vector.h
+++ thread_safe_vector/thread_safe_vector.h
@@ -12,25 +12,27 @@ class ThreadSafeVector {
   }
 
   T operator[](size_t index) const {
-    std::shared_lock<std::shared_mutex> r_lock(block_);
+    std::shared_lock r_lock(block_);
     return vector_[index];
   }
 
   size_t Size() const {
-    std::shared_lock<std::shared_mutex> r_lock(block_);
+    std::shared_lock r_lock(block_);
     return vector_.size();
   }
 
   void PushBack(const T& value) {
-    std::shared_lock<std::shared_mutex> w_lock(block_);
-    if (Size() < vector_.capacity()) {
-      
+    std::unique_lock wlocker(write_block_);
+    std::unique_lock rwlocker(block_, std::defer_lock_t());
+    if (Size() == vector_.capacity()) {
+      rwlocker.lock();
     }
     vector_.push_back(value);
   }
 
  private:
   mutable std::shared_mutex block_;
+  std::mutex write_block_;
   std::mutex pb_block_;
   std::vector<T> vector_;
 };
