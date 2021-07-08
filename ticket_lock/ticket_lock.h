74d3002fa6ff53967aa52b1f3c7fd262f0a1d440
pasha <pavel.guskov02@mail.ru>
2021-07-08 23:07:47 +0300

    changed naming

--- ticket_lock/ticket_lock.h
+++ ticket_lock/ticket_lock.h
@@ -9,18 +9,18 @@ class TicketLock {
   }
 
   void Lock() {
-    size_t cur_id = tickets_cnt.fetch_add(1);
-    while (now_serving.load() != cur_id) {
+    size_t cur_id = tickets_cnt_.fetch_add(1);
+    while (now_serving_.load() != cur_id) {
       std::this_thread::yield();
     }
   }
 
   void Unlock() {
-    now_serving.fetch_add(1);
+    now_serving_.fetch_add(1);
   }
 
  private:
-  std::atomic<size_t> tickets_cnt = 0;
-  std::atomic<size_t> now_serving = 0;
+  std::atomic<size_t> tickets_cnt_ = 0;
+  std::atomic<size_t> now_serving_ = 0;
 };
 
