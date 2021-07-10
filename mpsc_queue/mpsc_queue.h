eb897d30d40a0c8915a818bb14fb70dc825d008b
pasha <pavel.guskov02@mail.ru>
2021-07-10 19:02:27 +0300

    fixed naming

--- mpsc_queue/mpsc_queue.h
+++ mpsc_queue/mpsc_queue.h
@@ -9,35 +9,35 @@ class MPSCQueue {
   MPSCQueue() = default;
 
   ~MPSCQueue() {
-    auto current_head = head.load();
+    auto current_head = head_.load();
     while (current_head) {
-      head.store(current_head->next);
+      head_.store(current_head->next);
       delete current_head;
-      current_head = head.load();
+      current_head = head_.load();
     }
   }
 
   void Push(const T& value) {
     Node* ptr_node = new Node(value);
     for (;;) {
-      ptr_node->next.store(head.load());
+      ptr_node->next.store(head_.load());
       Node* cpy = ptr_node->next.load();
-      if (head.compare_exchange_weak(cpy, ptr_node)) {
+      if (head_.compare_exchange_weak(cpy, ptr_node)) {
         return;
       }
     }
   }
 
   std::optional<T> Pop() {
-    auto current_head = head.load();
+    auto current_head = head_.load();
     for (;;) {
       if (!current_head) {
         return std::nullopt;
       }
-      if (head.compare_exchange_weak(current_head, current_head->next.load())) {
+      if (head_.compare_exchange_weak(current_head, current_head->next.load())) {
         break;
       }
-      current_head = head.load();
+      current_head = head_.load();
     }
     std::optional<T> result(current_head->element.load());
     delete current_head;
@@ -55,6 +55,6 @@ class MPSCQueue {
     Node(const T& value) : next(nullptr), element(value) {}
   };
 
-  std::atomic<Node*> head{nullptr};
+  std::atomic<Node*> head_{nullptr};
 };
 
