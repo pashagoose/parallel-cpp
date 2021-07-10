1353702b4cabee895498537888689043d53a6da5
pasha <pavel.guskov02@mail.ru>
2021-07-10 18:59:57 +0300

    solving mpmc_queue

--- mpmc_queue/mpmc_queue.h
+++ mpmc_queue/mpmc_queue.h
@@ -7,8 +7,12 @@
 template <class T>
 class MPMCQueue {
  public:
-  explicit MPMCQueue(uint64_t /*max_size*/) {
-    // Your code
+  explicit MPMCQueue(uint64_t max_size) {
+    uint64_t good_size = 1;
+    while (good_size < max_size) {
+      good_size *= 2;
+    }
+    array.resize(good_size);
   }
 
   bool Push(const T& /*value*/) {
@@ -22,6 +26,14 @@ class MPMCQueue {
   }
 
  private:
-  // Your code
+    
+    struct Node {
+      std::atomic<T> value;
+      std::atomic<uint64_t> generation;
+    }
+
+    std::vector<Node> array_;
+    std::atomic<uint64_t> head_ = static_cast<uint64_t> (-1);
+    std::atomic<uint64_t> tail_ = 0;
 };
 
